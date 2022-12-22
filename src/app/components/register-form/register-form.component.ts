e358ca3bcef4009883a6020586b9b65af8b4bcaa
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2022-12-22 16:17:16 +0400

    Updated register-form

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { FirebaseWorkerService } from '../../services/firebase-worker.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -7,10 +10,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
+  userInfo: User = new User();
 
-  constructor(private router: Router) { }
+  checkInpType: boolean = true;
+  eyeIcon: string = 'ri-eye-off-fill';
+  inpType: string = 'password';
+
+
+  constructor(private router: Router,
+    private fireWorker:FirebaseWorkerService) { }
 
   ngOnInit(): void {
   }
 
+  onFormSubmit(form: NgForm) {
+    var tmpUser = Object.assign(new User(), form.value);
+    this.fireWorker.signUp(tmpUser, form.value.password).then((response: any) => {
+      this.router.navigate(['/authorization/login']);
+      console.log(response);
+    })
+  }
+
+  eyeIconToggle() {
+    this.checkInpType = !this.checkInpType;
+    if (this.checkInpType) {
+      this.eyeIcon = 'ri-eye-off-fill';
+      this.inpType = 'password';
+    } else {
+      this.eyeIcon = 'ri-eye-fill';
+      this.inpType = 'text';
+    }
+  }
 }
